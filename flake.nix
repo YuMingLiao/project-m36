fe19f55fed08f8b22f5df45d464c60c8379f0db5
Yu-Ming Liao <dycan0716@gmail.com>
2024-02-08 22:38:30 +0800

    bulitins.getFlake experiemental features: repl flake
    I can cabal build project-m36 in devShell now! Ya!
    (nix develop --impure)
    
    ghci interprets fine.
    ghci -fobject-code, not.
    cabal repl with -fplugin=Graph.Trace => type class debug_ip
    
    ok: ghci ProjectM36/Tuple.hs -fobject-code -fplugin=Graph.Trace  -XCPP -XOverloadedStrings -XLambdaCase
    ok: ghc ProjectM36/Tuple.hs -fobject-code -fplugin=Graph.Trace  -XCPP -XOverloadedStrings -XLambdaCase
        (even add opt..., lazi..., cse...)
    nix build .#ghc924
    res: debug context connectionProjectM36

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
 
   outputs = { self, nixpkgs }:
+    with nixpkgs.legacyPackages.x86_64-linux;
     let
       config = {
         allowBroken = true;
@@ -23,23 +24,18 @@
               # enableLibraryProfiling = false;
               # enableExecutableProfiling = false;
             });
-          project-m36 = let
-            source = toSource {
-              root = ./.;
-              fileset = ./.git; # so that specific rev won't get rebuild due to unstaged files change.
-            };
-          in final.callCabal2nix "project-m36" source { };
+          project-m36 = final.callCabal2nix "project-m36" ./. { };
           barbies-th = doJailbreak prev.barbies-th;
           curryer-rpc = final.callHackageDirect {
             pkg = "curryer-rpc";
             ver = "0.3.5";
             sha256 = "sha256-7mEJOBKzA2rTnLxZme8E6zFv0VkiXBo5L/jUJSNPaNE=";
           } { };
-          streamly = final.callHackageDirect {
-            pkg = "streamly";
-            ver = "0.9.0";
-            sha256 = "sha256-eOxVb8qQjZDo1+S7CStqYSExOg2QHWkMY+zlOYqwZak=";
-          } { };
+          #streamly = final.callHackageDirect {
+          #  pkg = "streamly";
+          #  ver = "0.9.0";
+          #  sha256 = "sha256-eOxVb8qQjZDo1+S7CStqYSExOg2QHWkMY+zlOYqwZak=";
+          #} { };
           streamly-core = final.callHackageDirect {
             pkg = "streamly-core";
             ver = "0.1.0";
@@ -52,12 +48,52 @@
           } { };
 
         };
+      ghc963Overlay = with pkgs.haskell.lib;
+        with pkgs.lib.fileset;
+        final: prev: {
+          streamly-core = doJailbreak prev.streamly-core;
+          #stuck here
+          streamly = doJailbreak prev.streamly;
+
+        };
 
       myHaskellPackages = pkgs.haskellPackages.extend overlay;
+
+      t = lib.trivial;
+      hl = haskell.lib;
+
+      name = "project-m36";
+
+      project = ghc-version: devTools: # [1]
+        let addBuildTools = (t.flip hl.addBuildTools) devTools;
+        in haskell.packages.${ghc-version}.developPackage {
+          root = lib.sourceFilesBySuffices ./. [ ".cabal" ".hs" ];
+          name = name;
+          returnShellEnv = !(devTools == [ ]); # [2]
+
+          modifier = (t.flip t.pipe) [
+            addBuildTools
+            ((t.flip hl.addBuildTools) [cabal-install])
+            hl.dontHaddock
+            hl.enableStaticLibraries
+            hl.justStaticExecutables
+            hl.disableLibraryProfiling
+            hl.disableExecutableProfiling
+          ];
+        };
     in {
       defaultPackage.x86_64-linux = myHaskellPackages.project-m36;
-
-    };
+      packages.x86_64-linux.ghc963 =
+        ((pkgs.haskell.packages.ghc963.extend overlay).extend
+          ghc963Overlay).project-m36;
+      packages.x86_64-linux.ghc948 =
+        (pkgs.haskell.packages.ghc948.extend overlay).project-m36;
+      packages.x86_64-linux.ghc944 =
+        (pkgs.haskell.packages.ghc944.extend overlay).project-m36;
+      packages.x86_64-linux.ghc924 =
+        (pkgs.haskell.packages.ghc924.extend overlay).project-m36;
+      devShells.x86_64-linux.default = project "ghc944" (pkgs.haskell.packages.ghc944.extend overlay).project-m36.getBuildInputs.haskellBuildInputs;
+      };
 }
 # Define apps that is triggered by `nix run` command. For example,
 # `nix run .#postgres` will run the script for postgres below
