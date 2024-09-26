fe4c7c5f0de7f80a834195572611ae2735ee45fc
YuMingLiao <dycan0716@gmail.com>
2024-09-26 13:21:09 +0000

    WIP: flake.nix

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  #inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
 
-  outputs = { self, nixpkgs}:
+  outputs = { self, nixpkgs }:
     with nixpkgs.legacyPackages.x86_64-linux;
     let
       config = {
@@ -10,7 +10,10 @@
       };
       pkgs =
         nixpkgs.legacyPackages.x86_64-linux.extend (f: p: { inherit config; });
-      repo = builtins.path { path = /root/project-m36-on-last-nixos/project-m36; name = "project-m36"; };
+      repo = builtins.path {
+        path = ./.;
+        name = "project-m36";
+      };
       overlay = with pkgs.haskell.lib;
         with pkgs.lib.fileset;
         final: prev: {
@@ -18,16 +21,30 @@
           # IFD (https://nixos.wiki/wiki/Import_From_Derivation).
           # Might be possible to use in the near future, check this out:
           # https://github.com/cdepillabout/cabal2nixWithoutIFD
-#          mkDerivation = drv:
-#            prev.mkDerivation (drv // {
-#              doCheck = false;
-#              doHaddock = false;
-#              enableLibraryProfiling = false;
-#              enableExecutableProfiling = false;
-#            });
-          project-m36-full = final.callCabal2nix "project-m36" repo { };
-          project-m36 = compose.setBuildTargets ["lib:project-m36" "exe:project-m36-server" "exe:tutd"] final.project-m36-full; 
-          barbies-th = doJailbreak prev.barbies-th;
+
+          mkDerivation = drv:
+            if drv.pname == "hscolour" then
+              drv
+            else
+              prev.mkDerivation (drv // {
+                doCheck = false;
+                doHaddock = false;
+                enableLibraryProfiling = false;
+                enableExecutableProfiling = false;
+              });
+          project-m36-full =
+            pkgs.lib.pipe (final.callCabal2nix "project-m36" repo { }) [
+              dontCheck
+              dontHaddock
+              disableLibraryProfiling
+              disableExecutableProfiling
+            ];
+          project-m36 = compose.setBuildTargets [
+            "lib:project-m36"
+            "exe:project-m36-server"
+            "exe:tutd"
+          ] final.project-m36-full;
+          #barbies-th = doJailbreak prev.barbies-th;
           curryer-rpc = final.callHackageDirect {
             pkg = "curryer-rpc";
             ver = "0.3.5";
@@ -38,24 +55,24 @@
           #  ver = "0.9.0";
           #  sha256 = "sha256-eOxVb8qQjZDo1+S7CStqYSExOg2QHWkMY+zlOYqwZak=";
           #} { };
-#          streamly-core = final.callHackageDirect {
-#            pkg = "streamly-core";
-#            ver = "0.1.0";
-#            sha256 = "sha256-hoSV6Q2+X5a7hFnJAArqNPjcMaCVyX9Vz4FcxeJ+jgI=";
-#          } { };
-          streamly-bytestring = final.callHackageDirect {
-            pkg = "streamly-bytestring";
-            ver = "0.2.1";
-            sha256 = "sha256-EcH6qq4nRjea3xQ66Zlqgjjg7lF/grkKJI0+tTO4B84=";
-          } { };
+          #          streamly-core = final.callHackageDirect {
+          #            pkg = "streamly-core";
+          #            ver = "0.1.0";
+          #            sha256 = "sha256-hoSV6Q2+X5a7hFnJAArqNPjcMaCVyX9Vz4FcxeJ+jgI=";
+          #          } { };
+          #streamly-bytestring = final.callHackageDirect {
+          #  pkg = "streamly-bytestring";
+          #  ver = "0.2.1";
+          #  sha256 = "sha256-EcH6qq4nRjea3xQ66Zlqgjjg7lF/grkKJI0+tTO4B84=";
+          #} { };
 
         };
-      myHaskellPackages = pkgs.haskellPackages.extend overlay;
+      myHaskellPackages = pkgs.haskell.packages.ghc948.extend overlay;
 
-   in {
-       a = (pkgs.haskell.packages.ghc944.extend overlay).hscolour;
+    in {
+      #a = (pkgs.haskell.packages.ghc944.extend overlay).hscolour;
       defaultPackage.x86_64-linux = myHaskellPackages.project-m36;
-      packages.x86_64-linux.ghc944 =
-        (pkgs.haskell.packages.ghc944.extend overlay).project-m36;
-      };
+      #packages.x86_64-linux.ghc944 =
+      #  (pkgs.haskell.packages.ghc944.extend overlay).project-m36;
+    };
 }
