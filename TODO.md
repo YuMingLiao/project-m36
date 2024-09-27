fd54f9b00b3aca1f741c65d8f6f1c6569894da11
YuMingLiao <dycan0716@gmail.com>
2024-09-27 11:19:05 +0000

    use haskell-flake to build prjoect-m36
    fix two packages in nixos-24.05 in myhaskell.nix,
    
    flakes can't input repos in subfolder for now.
    
    nixpkgs or flake, both can overlay.
    It's just ... update or not, that's the catch.
    
    haskell-flake is the best so far, good at overlay.

---
+++ TODO.md
@@ -0,0 +1,4 @@
+[x] ghc948 failed at missing foldabl1-class-compat for lattices
+[x] ghc965 winery failed at SChar conllision.
+[] flake can't use submodule streamly-core
+
--- flake.nix
+++ flake.nix
@@ -1,78 +1,103 @@
 {
-  #inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  inputs = rec {
+    # from niv
+    # nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/9fafaa30660e204bb27a35b3c608f03609705a5d.tar.gz";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+   streamly-bytestring = {
+      type = "github";
+      owner = "psibi";
+      repo = "streamly-bytestring";
+      flake = false;
+    };
+    scotty.url = "github:scotty-web/scotty";
+    scotty.flake = false;
+  };
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      streamly-bytestring,
+      scotty,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [ inputs.haskell-flake.flakeModule ];
 
-  outputs = { self, nixpkgs }:
-    with nixpkgs.legacyPackages.x86_64-linux;
-    let
-      config = {
-        allowBroken = true;
-        allowAliases = false;
-      };
-      pkgs =
-        nixpkgs.legacyPackages.x86_64-linux.extend (f: p: { inherit config; });
-      repo = builtins.path {
-        path = ./.;
-        name = "project-m36";
-      };
-      overlay = with pkgs.haskell.lib;
-        with pkgs.lib.fileset;
-        final: prev: {
-          # `nix flake show` and `nix flake check` won't work because of
-          # IFD (https://nixos.wiki/wiki/Import_From_Derivation).
-          # Might be possible to use in the near future, check this out:
-          # https://github.com/cdepillabout/cabal2nixWithoutIFD
+      perSystem =
+        {
+          self',
+          pkgs,
+          config,
+          ...
+        }:
+        {
 
-          mkDerivation = drv:
-            if drv.pname == "hscolour" then
-              drv
-            else
-              prev.mkDerivation (drv // {
-                doCheck = false;
-                doHaddock = false;
-                enableLibraryProfiling = false;
-                enableExecutableProfiling = false;
-              });
-          project-m36-full =
-            pkgs.lib.pipe (final.callCabal2nix "project-m36" repo { }) [
-              dontCheck
-              dontHaddock
-              disableLibraryProfiling
-              disableExecutableProfiling
-            ];
-          project-m36 = compose.setBuildTargets [
-            "lib:project-m36"
-            "exe:project-m36-server"
-            "exe:tutd"
-          ] final.project-m36-full;
-          #barbies-th = doJailbreak prev.barbies-th;
-          curryer-rpc = final.callHackageDirect {
-            pkg = "curryer-rpc";
-            ver = "0.3.5";
-            sha256 = "sha256-7mEJOBKzA2rTnLxZme8E6zFv0VkiXBo5L/jUJSNPaNE=";
-          } { };
-          #streamly = final.callHackageDirect {
-          #  pkg = "streamly";
-          #  ver = "0.9.0";
-          #  sha256 = "sha256-eOxVb8qQjZDo1+S7CStqYSExOg2QHWkMY+zlOYqwZak=";
-          #} { };
-          #          streamly-core = final.callHackageDirect {
-          #            pkg = "streamly-core";
-          #            ver = "0.1.0";
-          #            sha256 = "sha256-hoSV6Q2+X5a7hFnJAArqNPjcMaCVyX9Vz4FcxeJ+jgI=";
-          #          } { };
-          #streamly-bytestring = final.callHackageDirect {
-          #  pkg = "streamly-bytestring";
-          #  ver = "0.2.1";
-          #  sha256 = "sha256-EcH6qq4nRjea3xQ66Zlqgjjg7lF/grkKJI0+tTO4B84=";
-          #} { };
+          # Typically, you just want a single project named "default". But
+          # multiple projects are also possible, each using different GHC version.
+          haskellProjects.ghc965 = {
+            defaults.packages = { }; # no local packages
+            devShell.enable = false;
+            autoWire = [];
+            basePackages = pkgs.haskell.packages.ghc965;
+            packages =
+              let
+                self = pkgs.haskell.packages.ghc965;
+              in
+              {
+                streamly-bytestring.source = streamly-bytestring;
+                scotty.source = scotty;
+              };
+            settings = {
+              streamly-bytestring.jailbreak = true;
+              streamly.jailbreak = true;
+              curryer-rpc.jailbreak = true;
+            };
+          };
 
+          haskellProjects.default = {
+            # The base package set representing a specific GHC version.
+            # By default, this is pkgs.haskellPackages.
+            # You may also create your own. See https://community.flake.parts/haskell-flake/package-set
+            basePackages = config.haskellProjects.ghc965.outputs.finalPackages;
+
+            # Extra package information. See https://community.flake.parts/haskell-flake/dependency
+            #
+            # Note that local packages are automatically included in `packages`
+            # (defined by `defaults.packages` option).
+            #defaults.packages = {};
+            packages = {
+              # aeson.source = "1.5.0.0";      # Override aeson to a custom version from Hackage
+              # shower.source = inputs.shower; # Override shower to a custom source path
+            };
+            settings = {
+              #  aeson = {
+              #    check = false;
+              #  };
+              #  relude = {
+              #    haddock = false;
+              #    broken = false;
+              #  };
+            };
+
+            devShell = {
+              # Enabled by default
+              # enable = true;
+
+              # Programs you want to make available in the shell.
+              # Default programs can be disabled by setting to 'null'
+              # tools = hp: { fourmolu = hp.fourmolu; ghcid = null; };
+
+              # Check that haskell-language-server works
+              # hlsCheck.enable = true; # Requires sandbox to be disabled
+            };
+          };
+
+          # haskell-flake doesn't set the default package, but you can do it here.
+          # packages.default = config.haskellProjects.ghc948.outputs.finalPackages.lattices;
+          packages.default = self'.packages.project-m36;
         };
-      myHaskellPackages = pkgs.haskell.packages.ghc948.extend overlay;
-
-    in {
-      #a = (pkgs.haskell.packages.ghc944.extend overlay).hscolour;
-      defaultPackage.x86_64-linux = myHaskellPackages.project-m36;
-      #packages.x86_64-linux.ghc944 =
-      #  (pkgs.haskell.packages.ghc944.extend overlay).project-m36;
     };
 }
